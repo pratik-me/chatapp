009adb46c8be0f1caa5ec0e1ffb7e097a72c201b
Pratik Kumar <pk.rpvv@gmail.com>
2026-03-29 23:13:55 +0530

    Added cloudinary

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
-import type { Request, Response } from "express"
+import type { NextFunction, Request, Response } from "express"
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils/cookies.js";
 import { sendEmail } from "../lib/email/emailRender.js";
+import cloudinary from "../lib/cloudinary/index.js";
 import prisma from "../../prisma/index.js";
 
 export const signup = async (req: Request, res: Response) => {
@@ -75,4 +76,26 @@ export const login = async (req: Request, res: Response) => {
 export const logout = (req: Request, res: Response) => {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
+}
+
+export const updateProfile = async (req: any, res: Response, next: NextFunction) => {
+    try {
+        const {profilePic} = req.body;
+        if(!profilePic) return res.status(400).json({message: "Profile Picture is required"});
+        const userId: string = req.user.id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await prisma.user.update({
+            where: {id: userId},
+            data: {
+                profilePic: uploadResponse.secure_url
+            }
+        })
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in updateProfile controller:\n", error);
+        res.status(500).json({message: "Internal Server error"})
+    }
 }
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { isAuthenticated } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout);  // POST to prevent unauthorized state changes and protect against CSRF attacks.
 
-// router.put("/update-profile", isAuthenticated, updateProfile);
+router.put("/update-profile", isAuthenticated, updateProfile);
+router.get("/check", isAuthenticated, (req: any, res: Response) => res.status(200).json(req.user))
 
 export default router;
